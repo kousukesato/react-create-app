e2bb5cf403fc15e391e09c2ab51011bed6954d94
EC2 Default User <ec2-user@ip-172-31-34-163.us-east-2.compute.internal>
2021-09-11 15:34:23 +0000

    reactのhook

---
+++ src/componets/ApiFetch.js
@@ -0,0 +1,39 @@
+import React,　{useState,useEffect} from 'react'
+import axios from 'axios'
+
+
+const ApiFetch=()=>{
+    
+       const[posts, setPosts]= useState([])
+       const[id,setId]= useState(1)
+       const[clicked,setClicked]= useState(false)
+       
+       const handlerClicked=()=>{
+           setClicked(!clicked)
+       }
+       
+       
+       useEffect(()=> {
+           fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{method:'GET'})
+           .then(res=> res.json())
+           .then(data =>{
+               setPosts(data)
+            })
+           
+           
+       },[clicked])
+              
+       return(
+          <div>
+          <input type='text' value={id} onChange={evt =>setId(evt.target.value)}/>
+          <br />
+          <button type='button' onClick={handlerClicked}>get post</button>
+           <br/>
+           {posts.title}
+           
+           
+           </div>
+        )
+    }
+
+export default ApiFetch
